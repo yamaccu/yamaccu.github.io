219c93b1267f301edc04561927bd6e981862410b
yamaccu <yus1206@hotmail.com>
2021-11-24 22:58:59 +0900

    change layout

---
+++ js/tils.js
@@ -0,0 +1,27 @@
+//jQuery
+
+$(function(){
+  $(".menu").click(function()
+  {
+    if(sidemenu.style.left== "5px")
+    {
+      sidemenu.style.left = "-65%";
+    }
+    else
+    {
+      sidemenu.style.left = "5px";
+    }
+  });
+
+  $(window).resize(function()
+  {
+    if ($(window).width() >= 768) 
+    {
+      sidemenu.style.left = "5px";
+    }
+    else
+    {
+      sidemenu.style.left = "-65%";
+    }
+  });
+});
