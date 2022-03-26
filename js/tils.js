84fee7b24d34daee08ee6efb501a7b5e3120304e
yamaccu <yus1206@hotmail.com>
2022-03-26 18:46:10 +0900

    add article

--- js/tils.js
+++ js/tils.js
@@ -1,5 +1,39 @@
+
+window.onresize = resizeWindow;
+
+window.onload = function () {
+  console.log("onload");
+  var humberger = document.getElementById("humbergericon");
+  humberger.onclick = sideMenu;
+}
+
+function resizeWindow(){
+  if (window.innerWidth >= 768) 
+  {
+    sidemenu.style.left = "5px";
+  }
+  else
+  {
+    sidemenu.style.left = "-100%";
+  }
+}
+
+function sideMenu() {
+  if(sidemenu.style.left== "5px")
+  {
+    sidemenu.style.left = "-100%";
+  }
+  else
+  {
+    sidemenu.style.left = "5px";
+  }
+}
+
+
+
 //jQuery
 
+/*
 $(function(){
   $(".menu").click(function()
   {
@@ -25,3 +59,4 @@ $(function(){
     }
   });
 });
+    */
